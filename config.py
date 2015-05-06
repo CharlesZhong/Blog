0e49d124c275970c13f1e33ccde8cd1080b38f03
huatianjue@gmail.com <Charles@zx.local>
2015-05-06 18:21:31 +0800

    fix mail bug

--- config.py
+++ config.py
@@ -6,14 +6,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'nothing is impossible'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    MAIL_SERVER = 'smtp.163.com'
+    MAIL_SERVER = 'smtp.sina.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
     AFG_BLOG_MAIL_SUBJECT_PREFIX = '[AimForGoogleBlog]'
-    AFG_BLOG_MAIL_SENDER = 'AFG Blog Admin <aimforgoogle@google.com>'
+    AFG_BLOG_MAIL_SENDER = 'AFG Blog Admin <aimforgoogle@sina.com>'
     AFG_BLOG_ADMIN = os.environ.get('AFG_ADMIN')
 
     @staticmethod
