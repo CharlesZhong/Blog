f37847d4e95d916773745f044ac38323abd28092
Xin Zhong <Charles@zx.local>
2015-05-06 15:36:24 +0800

    First Line!

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Blog
-A Blog By Flask.
+Blog
+======
+
+This repository contains the source code my Blog [Aim For Google](http://www.aimforgoogle.com).
+
+
---
+++ config.py
@@ -0,0 +1,45 @@
+__author__ = 'Charles'
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'nothing is impossible'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+    AFG_BLOG_MAIL_SUBJECT_PREFIX = '[AimForGoogleBlog]'
+    AFG_BLOG_MAIL_SENDER = 'AFG Blog Admin <aimforgoogle@google.com>'
+    AFG_BLOG_ADMIN = os.environ.get('AFG_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
